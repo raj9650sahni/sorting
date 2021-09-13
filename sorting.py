2949bd0dbe921cf5565c828a84bae61e022cad3e
Raj Kishore Sahni <63728002+raj9650sahni@users.noreply.github.com>
2021-09-13 17:31:55 +0530

    Add files via upload

---
+++ sorting.py
@@ -0,0 +1,23 @@
+
+
+
+def partition(arr,low,high):
+   i = ( low-1 )
+   pivot = arr[high] 
+   for j in range(low , high):
+      if arr[j] <= pivot:
+         i = i+1
+         arr[i],arr[j] = arr[j],arr[i]
+   arr[i+1],arr[high] = arr[high],arr[i+1]
+   return i+1
+
+def quick(arr,low,high):
+   if low < high:
+      temp = partition(arr,low,high)
+      quick(arr, low, temp-1)
+      quick(arr, temp+1, high)
+
+arr = [3,10,4,7,1,9,45,87]
+n = len(arr)
+quick(arr,0,n-1)
+print(arr)
