cd529bc3abd4045651ba0626f43c645ac148a60c
Raj Kishore Sahni <63728002+raj9650sahni@users.noreply.github.com>
2021-09-14 11:46:14 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -2,38 +2,43 @@
 '''
 low = Starting index,  high = Ending index 
 quickSort(arr, low, high)
-{
+
     if (low < high)
-    {
+    
         temp is partitioning index, arr[temp] is not at bright place
         temp = partition(arr, low, high);
 
         quickSort(arr, low, temp - 1); --- Before temp
         quickSort(arr, temp + 1, high); --- After temp
-    }
-}
+    
+
 
 '''
 
 
 
-def partition(arr,low,high):
-   i = ( low-1 )
-   pivot = arr[high] 
-   for j in range(low , high):
-      if arr[j] <= pivot:
-         i = i+1
-         arr[i],arr[j] = arr[j],arr[i]
-   arr[i+1],arr[high] = arr[high],arr[i+1]
-   return i+1
+def qs(arr, l, r):
+    if l >= r:
+        return
+    p = partition(arr, l, r)
 
-def quick(arr,low,high):
-   if low < high:
-      temp = partition(arr,low,high)
-      quick(arr, low, temp-1)
-      quick(arr, temp+1, high)
+    qs(arr, l, p - 1)
+    qs(arr, p + 1, r)
 
-arr = [3,10,4,7,1,9,45,87]
-n = len(arr)
-quick(arr,0,n-1)
-print(arr)
+def partition(arr, l, r):
+    pivot = arr[r]
+    i = l - 1
+    for j in range(l, r):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[r] = arr[r], arr[i + 1]
+    return i + 1
+
+
+a1 = [3, -2, -1, 0, -6, 4, 1,-8]
+n = len(a1)
+
+
+qs(a1,0,n-1)
+print(a1)
